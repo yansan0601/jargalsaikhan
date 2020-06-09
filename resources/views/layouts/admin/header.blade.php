233e5cffd1d11e605b82557f4228a3104e6e1847
tseveendorj3285 <tseveendorj3285@gmail.com>
2020-06-09 10:39:02 +0800

    sidebar added

---
+++ resources/views/layouts/admin/header.blade.php
@@ -0,0 +1,59 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light navBar">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link pt-3" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+    </li>
+  </ul>
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item navElement">
+      <a>
+        <i class="fa fa-sign-out mr-2" aria-hidden="true"></i><span class="d-none d-sm-inline">Logout</span>
+      <a>
+    </li>
+  </ul>
+</nav>
+
+<aside class="main-sidebar sidebar-dark-info elevation-1 darkBlue">
+  <a>
+    <span class="brand-text font-weight-light"><b>Jargalsaikhan.mn</b></span>
+    </a>
+
+  <div class="sidebar">
+    <nav>
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item navItemHead py-3 pl-2">
+          <span><i class="fa fa-bars"></i> Navigation</span>
+        </li>
+
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fa fa-newspaper-o"></i>
+            <p>Мэдээ</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fa fa-newspaper-o"></i>
+            <p>Мэдээ</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fa fa-newspaper-o"></i>
+            <p>Мэдээ</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fa fa-newspaper-o"></i>
+            <p>Мэдээ</p>
+          </a>
+        </li>
+        <li class="mt-4 p-2"><a class= "btn btn-sm btn-block btn-danger text-white" >LogOut</a></li>
+      </ul>
+    </nav>
+  </div>
+</aside>
