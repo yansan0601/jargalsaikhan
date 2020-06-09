84a157188d7ec63f7a624fdfe3ddf95328dcc402
tseveendorj3285 <tseveendorj3285@gmail.com>
2020-06-09 17:09:53 +0800

    layout fixed

--- resources/views/layouts/admin/errors.blade.php
+++ resources/views/layouts/admin/errors.blade.php
@@ -1,10 +1,10 @@
 @if ($errors->any())
     <div class="alert alert-warning alert-dismissible"">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <ul>
         @foreach ($errors->all() as $error)
+        <ul>
             <li class="fa fa-warning"> {{ $error }} </li>
-        @endforeach
         </ul>
+        @endforeach
     </div>
 @endif
