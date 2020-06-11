86a3bdaceb0c3466187ab983966e3aadbe0bacd7
tseveendorj3285 <tseveendorj3285@gmail.com>
2020-06-11 17:10:33 +0800

    validate

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Нэвтрэх нэр эсвэл нууц үг буруу байна',
+    'throttle' => 'Хэт их оролдлого хийсэн тул түр хүлээнэ үү :seconds seconds.',
 
 ];
